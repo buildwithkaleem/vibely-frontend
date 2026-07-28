6d3992e75362297646a8d26dddcc10512f318866
ranakaleem786 <ranakaleem786422@gmail.com>
2026-07-28 08:24:46 +0500

    32 commit

--- components/upload/VideoPreview.jsx
+++ components/upload/VideoPreview.jsx
@@ -45,7 +45,7 @@ function VideoPreview({ file }) {
       <video
         src={previewUrl}
         controls
-        className="w-full max-w-sm rounded-xl border object-contain bg-black"
+        className="w-full max-w-md max-h-lg rounded-xl border object-contain bg-black"
       />
     </div>
 
