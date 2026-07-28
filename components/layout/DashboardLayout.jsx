f889ef210f4730f39e6b2bde3b651bb32afb03c5
ranakaleem786 <ranakaleem786422@gmail.com>
2026-07-28 17:06:09 +0500

    36 commit

--- components/layout/DashboardLayout.jsx
+++ components/layout/DashboardLayout.jsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
 
       <div className="flex-1">
 
-        <Navbar />
+        {/* <Navbar /> */}
 
         <main className="p-8 bg-gray-100 min-h-screen">
           {children}
