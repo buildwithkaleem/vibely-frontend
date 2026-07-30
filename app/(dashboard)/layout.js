33e3d4b766fdd4da05a88db64978ac60758e17bc
ranakaleem786 <ranakaleem786422@gmail.com>
2026-07-30 10:51:32 +0500

    39 commit

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,8 +1,10 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import Header from "@/components/dashboard/Header";
 
 export default function Layout({ children }) {
   return (
     <DashboardLayout>
+      <Header/>
       {children}
     </DashboardLayout>
   );
